dd153b5a6b1bb0741aad29f566561b379955c20d
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-07-10 05:32:32 +0530

    created Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-center'>Hello</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+import Layout from "../components/Layout/Layout";
+
+export default function HomePage() {
+  return <Layout>HomePage</Layout>;
+}
